d4fecb8747156c3600003050135700b275a8a9c8
Kubakubikula <jakub.zient@gmail.com>
2021-11-11 18:06:12 +0100

    enhgancing model

--- api/weatherForecast/models.py
+++ api/weatherForecast/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Weather(models.Model):
-    created = models.DateField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
     date = models.DateField(auto_now_add=False)
-    country_code = models.CharField(max_length=2, blank=True, default='')
-    temperature = models.FloatField(blank=True, default=0.0)
+    country_code = models.CharField(max_length=2, blank=False)
+    temperature = models.FloatField(blank=False)
